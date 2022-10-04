ed34534d4e6698b4fea2d1db66166591fb2e00ae
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-04 20:38:23 +0530

    sm8250-common: Enable backpressure propagation in SF
    UI is visibly smoother now.
    
    While we're at it, since we are stuck with client
    composition due to unknown reasons, enable back the
    client composition cache to skip GPU composition
    when possible (https://github.com/AOSPA/android_frameworks_native/commit/9b079a2fef76ed1dfa69b158889edd0af5e6e52c)

--- common.mk
+++ common.mk
@@ -177,6 +177,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+PRODUCT_ODM_PROPERTIES += \
+    debug.sf.disable_client_composition_cache=0
+
 # Doze
 PRODUCT_PACKAGES += \
     OplusDoze
