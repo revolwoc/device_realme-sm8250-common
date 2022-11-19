dfc52fd27b438444e3621f33078e631870944c92
revolwoc <imsakshisharma04@gmail.com>
2022-11-19 02:21:48 +0000

    Revert "sm8250-common: Add GcamGo"
    This reverts commit 94b16512a8ad75fa5ced40ccf6dd5158137d82e0.

--- common.mk
+++ common.mk
@@ -129,8 +129,7 @@ PRODUCT_PACKAGES += \
     libstdc++.vendor
 
 PRODUCT_PACKAGES += \
-    Camera2 \
-    GcamGo
+    Camera2
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
