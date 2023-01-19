d73cfe8d3c793f5c644b057d6228e031b5c2baa8
revolwoc <imsakshisharma04@gmail.com>
2023-01-19 04:03:08 +0000

    sm8250-common: audio: Adapt audio_effects to Dolby
    Signed-off-by: revolwoc <imsakshisharma04@gmail.com>

--- common.mk
+++ common.mk
@@ -82,7 +82,7 @@ PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/kona/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(AUDIO_HAL_DIR)/configs/kona/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(AUDIO_HAL_DIR)/configs/kona/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc) \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc) \
 
 
 PRODUCT_COPY_FILES += \
