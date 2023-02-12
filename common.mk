3fcd71af7f951385547b9db26eab1ff4bc63051c
cristianvaz <cristianvaz81993@gmail.com>
2023-02-12 15:26:50 +0100

    sm8250-common: Adapt overlays to PixelOS

--- common.mk
+++ common.mk
@@ -330,7 +330,7 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
