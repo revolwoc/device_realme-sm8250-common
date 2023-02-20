9b02001a2dfcc910891b73a840d3337c83ae814a
Blackmanx <blackman@sajl.tk>
2023-02-20 12:13:36 +0100

    sm8250-common: Unlock aux cameras

--- extract-files.sh
+++ extract-files.sh
@@ -67,8 +67,7 @@ function blob_fixup() {
             sed -i "s|my_product|product|" "${2}"
             ;;
 		vendor/lib64/hw/com.qti.chi.override.so)
-            grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
-            sed -i "s/com.oem.autotest/\x00om.oem.autotest/" "${2}"
+            "${SIGSCAN}" -p "10 62 38 91" -P "1F 20 03 D5" -f "${2}"
             ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             "${SIGSCAN}" -p "AB 0B 00 94" -P "1F 20 03 D5" -f "${2}"
